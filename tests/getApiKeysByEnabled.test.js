d21b4faab2bf8b3e74ebf0efa0b4fd394535727a
brynaytes <brynaytes@gmail.com>
2023-09-04 12:47:48 -0500

    adding tests

--- tests/getApiKeysByEnabled.test.js
+++ tests/getApiKeysByEnabled.test.js
@@ -11,7 +11,36 @@ const exampleKeyObject = {
 }
 
 
-test('found enabled keys' , async () =>{
+const existingKeyObject  = {
+    "id": "nughmn2urh",
+    "name": "newKey2",
+    "enabled": true,
+    "createdDate": "2023-02-18T17:18:39-06:00",
+    "lastUpdatedDate": "2023-02-18T17:18:39-06:00",
+    "stageKeys": []
+}
+
+test('find enabled keys' , async () =>{
     const data = await gate.getApiKeysByEnabled(true);
     expect(data[0].enabled).toBe(true);
 })
+
+//This could fail on a new account
+test('find keys by partial name' , async () =>{
+    const data = await gate.getApiKeyObjectsByPartialName(existingKeyObject.name);
+    expect(data[0].name).toEqual(expect.stringMatching(existingKeyObject.name));
+})
+
+
+//This could fail on a new account
+test('find keys by full name' , async () =>{
+    const data = await gate.getApiKeyObjectsByName(existingKeyObject.name);
+    expect(data[0].name).toEqual(existingKeyObject.name);
+})
+
+//This could fail on a new account
+test('find keys by id' , async () =>{
+    const data = await gate.getApiKeyById(existingKeyObject.id);
+    expect(data.id).toEqual(existingKeyObject.id);
+})
+
